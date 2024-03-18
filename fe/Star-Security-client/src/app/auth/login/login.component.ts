30bc3586b663615fa158d6fe933e01154b1dc8f6
thanhlam212 <nguyenthanhlam02122003@gmail.com>
2024-03-18 23:27:58 +0700

    install axios

--- fe/Star-Security-client/src/app/auth/login/login.component.ts
+++ fe/Star-Security-client/src/app/auth/login/login.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  constructor() { }
 
+  ngOnInit(): void {
+
+  }
 }
