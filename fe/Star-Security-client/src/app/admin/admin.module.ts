5ef41127b5d0d12a07c4337082f5dea4d14ee318
Phatdoan203 <phatdoan.1014@gmail.com>
2024-03-19 10:27:55 +0700

    update 1.0

--- fe/Star-Security-client/src/app/admin/admin.module.ts
+++ fe/Star-Security-client/src/app/admin/admin.module.ts
@@ -2,10 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { MainComponent } from './featured/main/main.component';
+import { ProfileAdminComponent } from './featured/profile-admin/profile-admin.component';
+import { EmployeeComponent } from './featured/employee/employee.component';
+import { PaymentComponent } from './featured/payment/payment.component';
+import { CreateEmployeeComponent } from './featured/employee/create-employee/create-employee.component';
+import { EditEmployeeComponent } from './featured/employee/edit-employee/edit-employee.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NavbarComponent,
+    SidebarComponent,
+    MainComponent,
+    ProfileAdminComponent,
+    EmployeeComponent,
+    PaymentComponent,
+    CreateEmployeeComponent,
+    EditEmployeeComponent
+  ],
   imports: [
     CommonModule,
     AdminRoutingModule
