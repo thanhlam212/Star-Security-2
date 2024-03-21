97bace1d3bc86e76cabeae0ce6d88b64fd8c246b
thanhlam212 <nguyenthanhlam02122003@gmail.com>
2024-03-21 21:09:37 +0700

    nothing

--- be/bedotnet/StarSecurity/Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
+++ be/bedotnet/StarSecurity/Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+088607f12fbff1c15078ce749febb81c953e391b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- be/bedotnet/StarSecurity/application/obj/Debug/net8.0/application.AssemblyInfo.cs
+++ be/bedotnet/StarSecurity/application/obj/Debug/net8.0/application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+088607f12fbff1c15078ce749febb81c953e391b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
