daa8f0896e75cebd509ae05a281e4fd02c7f46e4
HuyNg <elvizhuy@gmail.com>
2024-03-27 22:56:19 +0700

    .

--- be/nodejs/app.js
+++ be/nodejs/app.js
@@ -1,3 +1,4 @@
+process.argv[1] = "--input-type=module";
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -5,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 import configDB from './config/configDB'
 var cors = require('cors');
-process.argv[1] = "--input-type=module";
 import configRedis from './config/configRedis'
 
 // var indexRouter = require('./routes/index');
