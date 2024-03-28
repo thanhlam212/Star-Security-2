29bdec15d6218c1687712f2f24e811e1064ed4ee
HuyNg <elvizhuy@gmail.com>
2024-03-28 19:34:36 +0700

    .

--- fe/Star-Security-client/src/app/environments/environment.ts
+++ fe/Star-Security-client/src/app/environments/environment.ts
@@ -2,5 +2,7 @@ export const environment = {
   production: false,
   apiUrl: 'https://localhost:7000',
   dotnetApiUrl: 'https://localhost:8081/api/v1',
-  nodejsApiUrl: 'http://localhost:8080',
+  // nodejsApiUrl: 'http://localhost:8080',
+  nodejsApiUrl: 'https://api-nodejs-star.huynn.io.vn',
+
 };
